aa2bc47339ffb42949e059ff3b6eb8536660b046
Only <ongranly@ngs.ru>
2022-12-18 20:06:54 +0700

    Курсовая, рефакторинг.

--- src/EmployeeBook.java
+++ src/EmployeeBook.java
@@ -1,8 +1,13 @@
+import java.util.LinkedList;
+
 public class EmployeeBook {
     private Employee[] employee = new Employee[10];
     public static final int departmentQuantity = 5; // количество отделов согласно условий задачи
 
     public boolean addEmployee(String fullName, int department, int salary) { // Добавляем сотрудника. Если добавился - ок, если нет места в массиве - ошибка
+        if (department <= 0 && department > EmployeeBook.departmentQuantity) {
+            return false;
+        }
         for (int i = 0; i < employee.length; i++) {
             if (employee[i] == null && department > 0 && department <= departmentQuantity) { // проверка на пустую ячейку в масиве и правильно введенный отдел
                 employee[i] = new Employee(fullName, department, salary);
@@ -31,42 +36,23 @@ public class EmployeeBook {
         return false;
     }
 
-    public void listAll() { // Выводим все данные по сотрудникам в консоль .toString
-        for (Employee emp : employee) {
-            if (emp != null) {
-                System.out.println(emp);
-            }
-        }
+    public Employee[] listAll() { // Вернуть все данные по сотрудникам для вывода в консоль .toString
+        return this.employee;
     }
 
-    public void listAll(int currentDepartment) { // Выводим данные сотрудников выбранного отдела
+    public LinkedList<Employee> listAll(int currentDepartment) { // Возвращаем данные сотрудников выбранного отдела
+        LinkedList<Employee> list = new LinkedList<>();
         for (Employee emp : employee) {
             if (emp != null) {
                 if (emp.getDepartment() == currentDepartment) {
-                    System.out.printf("id=%d, %s, зарплата %d\n", emp.getId(), emp.getFullName(), emp.getSalary());
+                    list.add(emp);
                 }
             }
         }
+        return list;
     }
 
-    public void listAllByDepartment() { // Вывод сотрудников по отделам
-        int counter = 0;
-        for (int currentDepartment = 1; currentDepartment <= departmentQuantity; currentDepartment++) {
-            System.out.println("Состав сотрудников отдела " + currentDepartment + ":");
-            for (Employee emp : employee) {
-                if (emp != null && emp.getDepartment() == currentDepartment) {
-                    System.out.println("    " + emp.getFullName());
-                    counter++;
-                }
-            }
-            if (counter == 0) {
-                System.out.println("  В отделе нет сотрудников.");
-            }
-            counter = 0;
-        }
-    }
-
-    public int monthSalary() { // Считаем месячный бюджет на зарплаты
+    public int getMonthSalary() { // Считаем месячный бюджет на зарплаты
         int totalMonthSalary = 0;
         for (Employee emp : employee) {
             if (emp != null) {
@@ -76,7 +62,7 @@ public class EmployeeBook {
         return totalMonthSalary;
     }
 
-    public int monthSalary(int currentDepartment) { // Считаем месячный бюджет отдела на зарплаты
+    public int getMonthSalary(int currentDepartment) { // Считаем месячный бюджет отдела на зарплаты
         int totalMonthSalary = 0;
         for (Employee emp : employee) {
             if (emp != null && emp.getDepartment() == currentDepartment) {
@@ -86,10 +72,10 @@ public class EmployeeBook {
         return totalMonthSalary;
     }
 
-    public Employee minSalaryEmployee() { // Ищем неудачника с минимальной зарплатой
+    public Employee getMinSalaryEmployee() { // Ищем неудачника с минимальной зарплатой
         Employee minSalaryEmployee = new Employee();
         for (int department = 1; department <= departmentQuantity; department++) {
-            Employee minSalaryByDepartment = minSalaryEmployee(department);
+            Employee minSalaryByDepartment = getMinSalaryEmployee(department);
             if (minSalaryEmployee.getFullName() == null || minSalaryEmployee.getSalary() > minSalaryByDepartment.getSalary()) {
                 minSalaryEmployee = minSalaryByDepartment;
             }
@@ -97,7 +83,7 @@ public class EmployeeBook {
         return minSalaryEmployee;
     }
 
-    public Employee minSalaryEmployee(int currentDepartment) { // Ищем неудачника в отделе с минимальной зарплатой
+    public Employee getMinSalaryEmployee(int currentDepartment) { // Ищем неудачника в отделе с минимальной зарплатой
         Employee minSalaryEmployee = new Employee();
         for (Employee emp : employee) {
             if (emp != null && emp.getDepartment() == currentDepartment) {
@@ -112,10 +98,10 @@ public class EmployeeBook {
         return minSalaryEmployee;
     }
 
-    public Employee maxSalaryEmployee() { // Ищем выскочку с максимальной зарплатой
+    public Employee getMaxSalaryEmployee() { // Ищем выскочку с максимальной зарплатой
         Employee maxSalaryEmployee = new Employee();
         for (int department = 1; department <= departmentQuantity; department++) {
-            Employee maxSalaryByDepartment = maxSalaryEmployee(department);
+            Employee maxSalaryByDepartment = getMaxSalaryEmployee(department);
             if (maxSalaryEmployee.getFullName() == null || maxSalaryEmployee.getSalary() < maxSalaryByDepartment.getSalary()) {
                 maxSalaryEmployee = maxSalaryByDepartment;
             }
@@ -123,7 +109,7 @@ public class EmployeeBook {
         return maxSalaryEmployee;
     }
 
-    public Employee maxSalaryEmployee(int currentDepartment) { // Ищем выскочку в отделе с максимальной зарплатой
+    public Employee getMaxSalaryEmployee(int currentDepartment) { // Ищем выскочку в отделе с максимальной зарплатой
         Employee maxSalaryEmployee = new Employee();
         for (Employee emp : employee) {
             if (emp != null && emp.getDepartment() == currentDepartment) {
@@ -137,37 +123,39 @@ public class EmployeeBook {
         return maxSalaryEmployee;
     }
 
-    public double averageSalary() { // Определяем среднюю зарплату по больнице
-        int totalSalary = monthSalary();
+    public double getAverageSalary() { // Определяем среднюю зарплату по больнице
+        int totalSalary = getMonthSalary();
         int employeeCounter = 0;
         for (Employee emp : employee) {
             if (emp != null) {
                 employeeCounter++;
             }
         }
-        return totalSalary / employeeCounter;
+        return (double) totalSalary / employeeCounter;
     }
 
-    public double averageSalary(int currentDepartment) { // Определяем среднюю зарплату по отделу
-        int totalSalary = monthSalary(currentDepartment);
+    public double getAverageSalary(int currentDepartment) { // Определяем среднюю зарплату по отделу
+        int totalSalary = getMonthSalary(currentDepartment);
         int employeeCounter = 0;
         for (Employee emp : employee) {
             if (emp != null && emp.getDepartment() == currentDepartment) {
                 employeeCounter++;
             }
         }
-        return totalSalary / employeeCounter;
+        return (double) totalSalary / employeeCounter;
     }
 
-    public void listAllFullName() { // Выводим ФЫО по всем сотрудника в консоль
+    public LinkedList<String> getAllFullName() { // Возвращаем ФЫО по всем сотрудникам
+        LinkedList<String> list = new LinkedList<>();
         for (Employee emp : employee) {
             if (emp != null) {
-                System.out.println(emp.getFullName());
+                list.add(emp.getFullName());
             }
         }
+        return list;
     }
 
-    public boolean raiseSalary(int percent) { // Индексируем зарплату на введеный процент
+    public boolean raiseSalary(int percent) { // Индексируем зарплату всем сотрудникам на введеный процент
         double k = (double) percent / 100 + 1;
         for (Employee value : employee) {
             if (value != null) {
@@ -187,22 +175,24 @@ public class EmployeeBook {
         return true;
     }
 
-    public void listLessSalary(int lessSalary) { // Выводим данные сотрудников, зарплата которых меньше указанной
-        System.out.printf("Сотрудники, зарплата которых меньше %d:\n", lessSalary);
+    public LinkedList<Employee> listLessSalary(int lessSalary) { // Возвращаем список сотрудников, зарплата которых меньше указанной
+        LinkedList<Employee> listLessSalary = new LinkedList<>();
         for (Employee emp : employee) {
             if (emp != null && emp.getSalary() < lessSalary) {
-                System.out.printf("id=%d,%s, зарплата: %d\n", emp.getId(), emp.getFullName(), emp.getSalary());
+                listLessSalary.add(emp);
             }
         }
+        return listLessSalary;
     }
 
-    public void listMoreSalary(int moreSalary) { // Выводим данные сотрудников, зарплата которых больше указанной
-        System.out.printf("Сотрудники, зарплата которых больше %d:\n", moreSalary);
+    public LinkedList<Employee> listMoreSalary(int moreSalary) { // Возвращаем список сотрудников, зарплата которых больше указанной
+        LinkedList<Employee> listMoreSalary = new LinkedList<>();
         for (Employee emp : employee) {
             if (emp != null && emp.getSalary() >= moreSalary) {
-                System.out.printf("id=%d,%s, зарплата: %d\n", emp.getId(), emp.getFullName(), emp.getSalary());
+                listMoreSalary.add(emp);
             }
         }
+        return listMoreSalary;
     }
 
     public boolean changeSalary(String fullName, int newSalary) { // Изменить ЗП указанному ФЫО
@@ -216,6 +206,9 @@ public class EmployeeBook {
     }
 
     public boolean changeDepartment(String fullName, int newDepartment) { // Изменить отдел указанному ФЫО
+        if (newDepartment <= 0 && newDepartment > EmployeeBook.departmentQuantity) { // Если введеный отдел вне допустимого диапазона - вываливаемся
+            return false;
+        }
         for (Employee value : employee) {
             if (value != null && value.getFullName().equals(fullName)) {
                 value.setDepartment(newDepartment);
